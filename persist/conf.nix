1544ad8cea19be069638eb35d4090ff29f99d852
Luk-ESC <eschbacher.lukas@gmail.com>
2025-06-13 23:06:19 +0200

    feat: persist zeditor

--- persist/conf.nix
+++ persist/conf.nix
@@ -17,11 +17,18 @@
     ".gnupg/"
     ".ssh/"
     ".local/share/keyrings/"
+
+    # ZEDITOR:
+    ".local/share/zed/"
+
   ];
 
   persist.location.caches.contents = [
     "/var/cache/"
     ".cache/"
+
+    # ZEDITOR:
+    ".local/share/zed/node/cache/"
 
     # ZSH
     ".zcompdump"
@@ -29,5 +36,9 @@
 
   persist.location.logs.contents = [
     "/var/log/"
+
+    # ZEDITOR:
+    ".local/share/zed/node/cache/_logs/"
+    ".local/share/zed/logs/"
   ];
 }
