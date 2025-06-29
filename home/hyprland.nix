bc4d6f0d3fe998e34eba1f5af2876e877498c401
Luk-ESC <eschbacher.lukas@gmail.com>
2025-06-29 21:10:50 +0200

    feat: add simple hyprland config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -2,15 +2,31 @@
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = false;
+    settings = {
+      "$mod" = "CTRL_SHIFT";
+      bind = [ "$mod, Q, exec, alacritty" ] ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]) 9));
+      input = {
+        kb_layout = "de";
+        touchpad = { natural_scroll = false; };
+      };
+    };
   };
 
-   programs.bash = {
-     enable = true;
-     profileExtra = ''
-       # Start hyprland from tty
-       if uwsm check may-start; then
-           exec uwsm start hyprland-uwsm.desktop
-       fi
-     '';
-   };
+  programs.bash = {
+    enable = true;
+    profileExtra = ''
+      # Start hyprland from tty
+      if uwsm check may-start; then
+          exec uwsm start hyprland-uwsm.desktop
+      fi
+    '';
+  };
 }
