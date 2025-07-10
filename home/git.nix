9cc5f6416097b518f987dec0b1c0fb2ebb146488
Luk-ESC <eschbacher.lukas@gmail.com>
2025-07-10 12:05:08 +0200

    perf(git): ditch gpg-agent for ssh signing

--- home/git.nix
+++ home/git.nix
@@ -1,49 +1,15 @@
-{ pkgs, lib, ... }: {
+{config, ...}: {
   programs.git = {
     enable = true;
 
     userName = "Luk-ESC";
     userEmail = "eschbacher.lukas@gmail.com";
-    signing.signByDefault = true;
+    signing = {
+      signByDefault = true;
+      key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      format = "ssh";
+    };
 
-    includes = [{
-      contents = {
-        user = {
-          name = "Luk-ESC";
-          email = "eschbacher.lukas@gmail.com";
-          signingKey = "D0C8FD2AB19DAAE6E658C40E780881B3A960B391";
-        };
-        commit.gpgSign = true;
-        init.defaultBranch = "main";
-      };
-    }];
-  };
-
-  programs.gpg = {
-    enable = true;
-    mutableTrust = false;
-    mutableKeys = false;
-    publicKeys = [{
-      source = ../keys/publickey.asc;
-      trust = 5;
-    }];
-  };
-
-  home.activation.importGpgKey = let privateKeyFile = ../keys/secretkeys.asc;
-  in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [ -f "${privateKeyFile}" ]; then
-      echo "Importing GPG private keys..."
-      ${lib.getExe pkgs.gnupg} --batch --import "${privateKeyFile}"
-      # Optionally, trust the keys automatically:
-      # echo -e "5\ny\n" | gpg --command-fd 0 --edit-key YOUR_KEY_ID trust
-    else
-      echo "No private key file found at ${privateKeyFile}, skipping import."
-    fi
-  '';
-
-  services.gpg-agent = {
-    enable = true;
-    enableZshIntegration = true;
-    pinentry.package = pkgs.pinentry-qt;
+    extraConfig.init.defaultBranch = "main";
   };
 }
