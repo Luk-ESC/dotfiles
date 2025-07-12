0662dbdf66b401345e48de8a392a56c446bb726f
Luk-ESC <eschbacher.lukas@gmail.com>
2025-07-13 01:45:39 +0200

    fix: fix nrs alias

--- home/zsh.nix
+++ home/zsh.nix
@@ -24,7 +24,7 @@
     '';
 
     shellAliases = {
-      nrs = "sudo nixos-rebuild switch";
+      nrs = "sudo nixos-rebuild switch --flake path:/home/eschb/nixcfg";
       ls = "ls --color=auto";
       reload = "source ~/.zshrc; rehash";
       m = "${
