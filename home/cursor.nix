b63b8e31cedf33d8f33d5419141b3868be11054a
Luk-ESC <eschbacher.lukas@gmail.com>
2025-07-18 14:34:22 +0200

    fix: change cursor size to 32

--- home/cursor.nix
+++ home/cursor.nix
@@ -2,7 +2,7 @@
   home.pointerCursor = {
     package = pkgs.rose-pine-hyprcursor;
     name = "rose-pine-hyprcursor";
-    size = 24;
+    size = 32;
     gtk.enable = true;
     hyprcursor.enable = true;
   };
