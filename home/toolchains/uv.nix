4c977d7192c5c349684b561f3c666435f366234d
Luk-ESC <eschbacher.lukas@gmail.com>
2025-08-25 00:35:41 +0200

    feat(toolchains-uv): configure zed lsps

--- home/toolchains/uv.nix
+++ home/toolchains/uv.nix
@@ -23,4 +23,30 @@ in
     enable = true;
     settings.cache-dir = config.xdg.cacheHome + "/ruff";
   };
+
+  programs.zed-editor = {
+    extensions = [ "ruff" ];
+    userSettings = {
+      languages.Python.language_servers = [
+        "pyright"
+        "ruff"
+      ];
+
+      lsp = {
+        ruff.binary = {
+          path = lib.getExe pkgs.ruff;
+
+          arguments = [
+            "server"
+            "--preview"
+          ];
+        };
+
+        pyright.binary = {
+          path = lib.getExe' pkgs.pyright "pyright-langserver";
+          arguments = [ "--stdio" ];
+        };
+      };
+    };
+  };
 }
