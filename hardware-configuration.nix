6af5f647d1f3c054f07e0f776e5c47639b521a60
Luk-ESC <eschbacher.lukas@gmail.com>
2025-08-29 12:54:37 +0200

    perf(hardware): switch to chronyd

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -27,6 +27,11 @@
   zramSwap.memoryPercent = 20; # ~3.2 GB on 16 GB RAM
   services.auto-epp.enable = true;
 
+  # Switch to chrony since it's preferred for laptops
+  services.chrony.enable = true;
+  persist.location.caches.contents = [ "/var/lib/chrony/" ];
+  services.timesyncd.enable = false;
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
