88f166535d781d9e9f4e4d563de21a454ca7e467
Luk-ESC <eschbacher.lukas@gmail.com>
2025-10-22 23:50:39 +0200

    feat(tools-zellij): configure quitting and scroll buffer size

--- home/tools/zellij.nix
+++ home/tools/zellij.nix
@@ -6,6 +6,10 @@
       show_startup_tips = false;
       show_release_notes = false;
       default_shell = "zsh";
+
+      on_force_close = "quit";
+      scroll_buffer_size = 100000;
+
       ui.pane_frames.rounded_corners = true;
     };
   };
