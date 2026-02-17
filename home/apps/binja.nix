7a2120544fc1ca512218946942a16c18c9a691e2
Luk-ESC <eschbacher.lukas@gmail.com>
2026-02-17 08:35:01 +0100

    feat(apps-binja): make persisting more fine-grained

--- home/apps/binja.nix
+++ home/apps/binja.nix
@@ -4,6 +4,11 @@
 
   persist.data.contents = [
     ".config/Vector 35/Binary Ninja.conf"
+    ".binaryninja/plugins/"
+    ".binaryninja/license.dat"
+  ];
+
+  persist.session.contents = [
     ".binaryninja/"
   ];
 }
