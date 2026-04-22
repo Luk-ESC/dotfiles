a40f12086479ed8b9eebe22b1c6bbd206e4c1a51
Luk-ESC <eschbacher.lukas@gmail.com>
2026-04-22 23:41:16 +0200

    feat(tools-codex): persist

--- home/tools/codex.nix
+++ home/tools/codex.nix
@@ -1,4 +1,17 @@
 { pkgs, lib, ... }:
 {
   home.shellAliases.codex = "${lib.getExe pkgs.bun} x @openai/codex";
+
+  # TODO: configure .codex/config.toml?
+  persist.session.contents = [
+    ".codex/"
+  ];
+
+  persist.caches.contents = [
+    ".codex/cache/"
+  ];
+
+  persist.logs.contents = [
+    ".codex/log/"
+  ];
 }
