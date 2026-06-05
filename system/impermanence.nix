6693b9803d2221196e7e59bc59b5e70722ec2734
Luk-ESC <eschbacher.lukas@gmail.com>
2026-06-05 14:39:31 +0200

    refactor(system-impermanence): fix timezone, get rid of sed

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -1,42 +1,42 @@
 {
   pkgs,
-  utils,
   lib,
+  config,
   ...
 }:
 {
-  boot.initrd.systemd.extraBin.sed = lib.getExe pkgs.gnused;
-  boot.initrd.systemd.services.impermanence-setup =
-    let
-      # TODO: this depends on diskos undocumented naming scheme :(
-      disk = "/dev/disk/by-partlabel/disk-main-root";
-    in
-    {
-      # Specify dependencies explicitly
-      unitConfig.DefaultDependencies = true;
-      # The script needs to run to completion before this service is done
-      serviceConfig.Type = "oneshot";
-      # This service is required for boot to succeed
-      requiredBy = [ "initrd.target" ];
-      # Should complete before any file systems are mounted
-      before = [ "sysroot.mount" ];
+  boot.initrd.systemd.services.impermanence-setup = {
+    # The script needs to run to completion before this service is done
+    serviceConfig.Type = "oneshot";
+    # This service is required for boot to succeed
+    requiredBy = [ "initrd.target" ];
+    # Should complete before any file systems are mounted
+    before = [ "sysroot.mount" ];
 
-      path = with pkgs; [
-        busybox
-        coreutils
-        btrfs-progs
-      ];
+    # TODO: are these the right services?
+    after = [
+      "initrd-root-device.target"
+      # Allow hibernation to resume before trying to alter any data
+      "local-fs-pre.target"
+    ];
 
-      # TODO: are these the right services?
-      after = [
-        "initrd-root-device.target"
-        # Allow hibernation to resume before trying to alter any data
-        "local-fs-pre.target"
-      ];
+    description = "Set up new root";
 
-      description = "Set up new root";
-
-      script = ''
+    script =
+      let
+        # TODO: this depends on diskos undocumented naming scheme :(
+        disk = "/dev/disk/by-partlabel/disk-main-root";
+        # TODO: this is really ugly
+        tz = lib.removeSuffix "\n" (
+          builtins.readFile (
+            pkgs.runCommandLocal "posix-tz" { } ''
+              tail -n 1 ${pkgs.tzdata}/share/zoneinfo/${config.time.timeZone} > $out
+            ''
+          )
+        );
+      in
+      # bash
+      ''
         mkdir /btrfs_tmp
         export PATH="/bin:$PATH"
 
@@ -46,21 +46,15 @@
             btrfs subvolume delete --recursive "$i"
         done
 
-        # TODO: fix timezone
-        timestamp=$(date "+%Y-%m-%d_%H:%M:%S")
+        timestamp=$(TZ="${tz}" date "+%Y-%m-%d_%H:%M:%S")
         new_root="/btrfs_tmp/$timestamp"
 
         btrfs subvolume create "$new_root"
-        # TODO: can i just use the subvolume path here, instead of goofy sed
-        id="$(btrfs subvolume show "$new_root" | sed -n 's/.*Subvolume ID:[[:space:]]*//p')"
+        btrfs subvolume set-default "$new_root"
 
         ln -sfn "$timestamp" /btrfs_tmp/current
 
         umount /btrfs_tmp
-
-        mount ${disk} /btrfs_tmp
-        btrfs subvolume set-default "$id" /btrfs_tmp
-        umount /btrfs_tmp
       '';
-    };
+  };
 }
