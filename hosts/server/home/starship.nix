bcf8e1e014736aae56853f506fbe84e40884430c
Luk-ESC <eschbacher.lukas@gmail.com>
2026-09-03 18:55:05 +0200

    fix(server-starship): set colors statically

---
+++ hosts/server/home/starship.nix
@@ -0,0 +1,11 @@
+{
+  programs.starship.settings = {
+    palette = "base16";
+    palettes.base16 = {
+      base07 = "#F3EFF8";
+      base04 = "#8B6FAE";
+      base01 = "#2C3040";
+      base00 = "#131217";
+    };
+  };
+}
